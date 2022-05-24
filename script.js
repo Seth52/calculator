8d8e4c1a7b7c816e4c0dee9c5bd9abbd69c43c0d
Seth <sethman5299@aol.com>
2022-05-23 21:38:15 -0400

    functions

---
+++ script.js
@@ -0,0 +1,20 @@
+let a;
+let b;
+
+function add(a,b){
+   let sum = a + b;
+    return sum;
+}
+function subtract(a,b){
+    let sum = a - b;
+    return sum;
+}
+function divide(a,b){
+    let sum = a/b;
+    return sum;
+
+}
+function multiply(a,b){
+    let sum= a * b;
+    return sum;
+}
